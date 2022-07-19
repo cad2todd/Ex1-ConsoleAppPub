4f57493af7cf1f4995572421fc0343fbe00d1039
Student@DDLS <Student@DDLS>
2022-07-18 18:35:49 -0700

    intial commit. hi, what is your name?

---
+++ Ex1-ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi. What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}. Nice to meet you");
+            Console.ReadKey();
+        }
+    }
+}
